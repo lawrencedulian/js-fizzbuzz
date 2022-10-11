601df119e20c7966bff1bac7fac390df963acffd
lawrencedulian <lawrenceedulian@gmail.com>
2022-10-11 15:30:59 +0200

    added bgClass

--- js/script.js
+++ js/script.js
@@ -7,17 +7,20 @@ const row = document.querySelector(".row");
 // STAMPARE NELLA CONSOLE NUMERI DA 1 A 100
 for (let i=1; i<=100; i++) {
     // SE i E' DIVISIBILE SIA PER 3 CHE PER 5 ALLORA STAMPA FIZZBUZZ
+     // SE i E' DIVISIBILE PER 3 STAMPA FIZZ ALTRIMENTI SE i E' DIVISIBILE PER 5 STAMPA BUZZ ALTRIMENTI STAMPA IL NUMERO
     let fizzBuzz = "";
+    let bgClass = "";
     if (i % 3 === 0 && i % 5 === 0 ) {
         fizzBuzz = "fizzBuzz";
+        bgClass = "fizzBuzz";
         console.log(fizzBuzz);
-    } else
-    // SE i E' DIVISIBILE PER 3 STAMPA FIZZ ALTRIMENTI SE i E' DIVISIBILE PER 5 STAMPA BUZZ ALTRIMENTI STAMPA IL NUMERO
-    if (i % 3 === 0) {
+    } else if (i % 3 === 0) {
         fizzBuzz = "fizz";
+        bgClass = "fizz";
         console.log(fizzBuzz);
     } else if (i % 5 === 0) {
         fizzBuzz = "buzz";
+        bgClass = "buzz";
         console.log(fizzBuzz);
     } else {
         fizzBuzz = i;
@@ -27,7 +30,12 @@ for (let i=1; i<=100; i++) {
     // OUTPUT
     const col = document.createElement("div");
     col.classList.add("col");
+   
+    if (bgClass !== "") {
+        col.classList.add(bgClass);
+    }
     col.innerHTML = fizzBuzz ;
+    
     row.append(col);
     
     // col = `<div class="col ${fizzBuzz}">${i}</div>`;
